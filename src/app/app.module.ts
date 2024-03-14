47dca1a42aa66ea3855317c3c7254ea87e887f33
yeisonsanz <yeisonkb@gmail.com>
2024-03-14 13:48:53 -0500

    se crearon los componentes compartidos shared y se crearon las listas del menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [//TODO: Declaraciones, directivas, componentes, pipes
+    AppComponent,
+   
   ],
-  imports: [
+  imports: [ //TODO: Solo se importan otros modulos
     BrowserModule,
     AppRoutingModule
   ],
